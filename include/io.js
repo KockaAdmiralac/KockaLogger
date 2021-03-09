a696bfd351d3eb37c9861b9f221502b2f9693397
Luka Simić <git@kocka.tech>
2021-03-09 15:23:31 +0100

    `got` rid of `request`.

--- include/io.js
+++ include/io.js
@@ -8,13 +8,23 @@
 /**
  * Importing modules
  */
-const http = require('request-promise-native'),
+const got = require('got'),
       util = require('./util.js');
 
 /**
  * HTTP communication handler.
  */
 class IO {
+    /**
+     * Class constructor. Creates the HTTP client shared by other methods.
+     */
+    constructor() {
+        this._client = got.extend({
+            headers: {
+                'User-Agent': util.USER_AGENT
+            }
+        });
+    }
     /**
      * Queries the MediaWiki API.
      * @param {String} wiki Wiki to query
@@ -24,44 +34,28 @@ class IO {
      * @param {Function} transform Transformation function
      * @returns {Promise} Promise to listen for response
      */
-    query(wiki, lang, domain, options, transform) {
+    async query(wiki, lang, domain, options, transform) {
         if (
             typeof wiki !== 'string' ||
             typeof options !== 'object'
         ) {
             return;
         }
-        options.action = 'query';
-        options.cb = Date.now();
-        options.format = 'json';
-        return http({
-            headers: {
-                'User-Agent': util.USER_AGENT
-            },
-            json: true,
-            method: 'GET',
-            qs: options,
-            transform,
-            uri: `${util.url(wiki, lang, domain)}/api.php`
-        });
-    }
-    /**
-     * Posts to a webhook.
-     * @param {String} url Webhook URL
-     * @param {Object} body POST body
-     * @returns {Promise} Promise to listen on for response
-     */
-    webhook(url, body) {
-        return http({
-            body,
-            headers: {
-                'Content-Type': 'application/json',
-                'User-Agent': util.USER_AGENT
-            },
-            json: true,
-            method: 'POST',
-            uri: url
-        });
+        const response = await this._client.get(
+            `${util.url(wiki, lang, domain)}/api.php`,
+            {
+                searchParams: {
+                    action: 'query',
+                    cb: Date.now(),
+                    format: 'json',
+                    ...options
+                }
+            }
+        ).json();
+        if (typeof transform === 'function') {
+            return transform(response);
+        }
+        return response;
     }
     /**
      * Gets user info for a user with specified user ID.
@@ -69,13 +63,14 @@ class IO {
      * @returns {Promise} Promise to listen on for response
      */
     userInfo(id) {
-        return http({
-            headers: {
-                'User-Agent': util.USER_AGENT
-            },
-            method: 'GET',
-            uri: `https://services.fandom.com/user-attribute/user/bulk?id=${id}`
-        });
+        return this._client.get(
+            `https://services.fandom.com/user-attribute/user/bulk?id=${id}`,
+            {
+                headers: {
+                    accept: '*/*'
+                }
+            }
+        ).json();
     }
 }
 
